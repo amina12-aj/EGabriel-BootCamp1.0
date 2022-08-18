0b59964aaf00b119e7c64e2dd02e1a0d766aac53
Onimcodes <Abrahammicheal55@yahoo.com>
2022-08-18 23:20:53 +0100

    attempted to fix conflicts

--- Micheal/First-Project/ProductsCRUDAPI/ProductsCRUDAPI/WeatherForecast.cs
+++ Micheal/First-Project/ProductsCRUDAPI/ProductsCRUDAPI/WeatherForecast.cs
@@ -1,6 +1,4 @@
-using System;
-
-namespace MichealsWebApp
+namespace ProductsCRUDAPI
 {
     public class WeatherForecast
     {
@@ -10,6 +8,6 @@ namespace MichealsWebApp
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
     }
 }
