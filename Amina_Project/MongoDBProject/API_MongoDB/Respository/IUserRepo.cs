a26d38e8c7b3b8eb722cb422fbc1964cd0eaa400
amina12-aj <bakareaminata@gmail.com>
2022-08-19 17:42:22 +0100

    Second_commit

--- Amina_Project/MongoDBProject/API_MongoDB/Respository/IUserRepo.cs
+++ Amina_Project/MongoDBProject/API_MongoDB/Respository/IUserRepo.cs
@@ -6,7 +6,11 @@ namespace API_MongoDB.Respository
     public interface IUserRepo
     {
         Task CreateUser(User user);
+       
+        Task<List<User>> GetAll();
+        Task <User> GetbyId (string Id);
+        Task <User> Update(string Id, User user);
+        
 
-      
     }
 }
