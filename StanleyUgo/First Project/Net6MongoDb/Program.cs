42e7cd60e945f1c9d31a65303db4a8876638ade8
Stanley-Ugo <iamkosmicblast@gmail.com>
2022-08-20 20:49:06 +0100

    Adding the POST User Method

--- StanleyUgo/First Project/Net6MongoDb/Program.cs
+++ StanleyUgo/First Project/Net6MongoDb/Program.cs
@@ -1,8 +1,14 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
 using Net6MongoDb.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+await DB.InitAsync("UserManagement",
+     MongoClientSettings.FromConnectionString(
+        "mongodb+srv://Stanley-Ugo:<scott4wheels>@usermanagementdb.pj7dqxz.mongodb.net/?retryWrites=true&w=majority"));
+
 builder.Services.AddScoped<IUserRepo, UserRepo>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
