00c69065135e1fc51c3892eae9992f39e1f3ce12
Stanley-Ugo <iamkosmicblast@gmail.com>
2022-08-20 19:17:11 +0100

    Adding the User Model

---
+++ StanleyUgo/First Project/Net6MongoDb/Models/User.cs
@@ -0,0 +1,18 @@
+using MongoDB.Entities;
+
+namespace Net6MongoDb.Models
+{
+    public class User : Entity
+    {
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public Gender Gender { get; set; }
+        public string Email { get; set; } = default!;
+    }
+
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2
+    }
+}
