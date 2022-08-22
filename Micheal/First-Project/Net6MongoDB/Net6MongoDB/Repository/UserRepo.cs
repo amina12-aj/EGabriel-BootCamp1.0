0e5d0936c9603b5da7911a7cfba2f6767e305e72
Onimcodes <Abrahammicheal55@yahoo.com>
2022-08-22 19:46:16 +0100

    Completed second Task

---
+++ Micheal/First-Project/Net6MongoDB/Net6MongoDB/Repository/UserRepo.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Entities;
+using Net6MongoDB.Models;
+
+namespace Net6MongoDB.Repository
+{
+    public class UserRepo:IuserRepo
+    {
+        public async Task CreateUser(User user)
+        {
+            await user.SaveAsync();
+
+        }
+
+        public async Task<List<User>> GetAll()
+        {
+            var users = await DB.Find<User>()
+                .Match(_ => true)
+                .ExecuteAsync();
+            return users;
+        }
+        public async Task<User> GetById(string Id)
+        {
+            var user = await DB.Find<User>()
+                .Match(x => x.ID == Id)
+                .ExecuteFirstAsync();
+            return user;
+        }
+        public async Task<User> UpdateUser(string Id,User user)
+        {
+            var updatedUser = await DB.UpdateAndGet<User>()
+                .MatchID(Id)
+                .ModifyWith(user)
+                .ExecuteAsync();
+            return updatedUser;
+        }
+
+    }
+    
+}
