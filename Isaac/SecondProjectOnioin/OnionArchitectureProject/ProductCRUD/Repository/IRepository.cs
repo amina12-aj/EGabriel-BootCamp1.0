1e6b2c4da122e6b032e4d019598f12ee0c6500f8
Adetunji Isaac <isaaccrown26@gmail.com>
2022-08-26 04:58:26 +0100

    Added Projects

---
+++ Isaac/SecondProjectOnioin/OnionArchitectureProject/ProductCRUD/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Get(int id);
+        IEnumerable<T> GetAll();
+
+    }
+}
