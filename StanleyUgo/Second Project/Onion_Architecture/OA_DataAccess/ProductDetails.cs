e86d9bc95a5d1d87c71c82380ec02548570cc45d
Stanley-Ugo <iamkosmicblast@gmail.com>
2022-08-27 11:36:43 +0100

    Adding Migrations and Updating Database

--- StanleyUgo/Second Project/Onion_Architecture/OA_DataAccess/ProductDetails.cs
+++ StanleyUgo/Second Project/Onion_Architecture/OA_DataAccess/ProductDetails.cs
@@ -9,5 +9,6 @@ namespace OA_DataAccess
         public int StockAvailable { get; set; }
         public decimal Price { get; set; }
         public virtual Product Product { get; set; }
+        public string ProductName { get; set; }
     }
 }
