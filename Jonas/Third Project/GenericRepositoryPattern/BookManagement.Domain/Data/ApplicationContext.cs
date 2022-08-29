4725fa8efff91a358961d60918fd0845cd1b4ffe
Jonas Ababio <jonasagesmilly@outlook.com>
2022-08-29 10:09:17 +0000

    Implemented Domain Layer

---
+++ Jonas/Third Project/GenericRepositoryPattern/BookManagement.Domain/Data/ApplicationContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManagement.Domain
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            _ = new AuthorMap(modelBuilder.Entity<Author>());
+            _ = new BookMap(modelBuilder.Entity<Book>());
+        }
+    }
+}
