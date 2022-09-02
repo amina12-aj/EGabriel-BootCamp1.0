b2963a50fa9e737a73c8a3803278ae0703993ac1
Adetunji Isaac <isaaccrown26@gmail.com>
2022-09-02 22:34:49 +0100

    Generic Repo Project

---
+++ Isaac/ThirdProject/GenericRepo/GR.Web/Models/CustomerListingViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GR.Web.Models
+{
+    
+        public class CustomerListingViewModel
+        {
+            public long Id { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string MobileNo { get; set; }
+        }
+    
+}
