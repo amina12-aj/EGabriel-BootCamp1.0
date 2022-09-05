b3111890843067ca7b450cdc454b7fbf3fad5b75
Onimcodes <Abrahammicheal55@yahoo.com>
2022-09-05 12:50:49 +0100

    COMPLETED TASK

---
+++ Micheal/Third-Project/Generics_Solution/Gr.Data/AuthorMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Gr.Data
+{
+    public class AuthorMap
+    {
+        public AuthorMap(EntityTypeBuilder<Author> entityBuilder)
+        {
+            entityBuilder.HasKey(t => t.Id);
+            entityBuilder.Property(t => t.FirstName).IsRequired();
+            entityBuilder.Property(t => t.LastName).IsRequired();
+            entityBuilder.Property(t => t.Email).IsRequired();
+        }
+    }
+}
