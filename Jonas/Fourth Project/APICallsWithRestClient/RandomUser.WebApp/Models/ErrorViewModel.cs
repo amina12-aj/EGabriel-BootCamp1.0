ceda71ea18936414760233c8379fc5da5e0f2358
Jonas Ababio <jonasagesmilly@outlook.com>
2022-09-08 21:13:15 +0000

    Added project files

---
+++ Jonas/Fourth Project/APICallsWithRestClient/RandomUser.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RandomUser.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
