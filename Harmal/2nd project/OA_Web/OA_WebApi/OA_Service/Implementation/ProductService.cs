0eb483a9bba6ac279b87710a3de86fead64eb523
hardeymolar <abdulmalikgbolahan95@gmail.com>
2022-09-10 03:08:41 +0100

    2nd project Completed

---
+++ Harmal/2nd project/OA_Web/OA_WebApi/OA_Service/Implementation/ProductService.cs
@@ -0,0 +1,32 @@
+﻿using OA_DataAccess;
+using OA_Repository;
+using OA_Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA_Service.Implementation
+{
+    public class ProductService : IProductService
+    {
+        private IRepository<Product> productRepository;
+        private IRepository<ProductDetails> productDetailRepository;
+
+        public ProductService(IRepository<Product> productRepository, IRepository<ProductDetails> productDetailRepository)
+        {
+            this.productRepository = productRepository;
+            this.productDetailRepository = productDetailRepository;
+        }
+        public IEnumerable<Product> GetProduct()
+        {
+            return productRepository.GetAll();
+        }
+
+        public Product GetProduct(int id)
+        {
+            return productRepository.Get(id);
+        }
+    }
+}
