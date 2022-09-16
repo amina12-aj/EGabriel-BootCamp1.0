a31806aa68aba0967632553b09c360423998a142
amina12-aj <bakareaminata@gmail.com>
2022-09-16 19:34:01 +0100

    API Consume

---
+++ Group One/First Project/CocktailApplication.Domain/Models/CockTail.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace CocktailApplication.Presentation.Models
+{
+    public class CockTail
+    {
+
+          [JsonProperty("drinks")]
+        public Dictionary<string, string>[] Drinks { get; set; } = default;
+        
+    }
+}
---
+++ Group One/First Project/CocktailApplication.Repository/IConsume.cs
@@ -0,0 +1,16 @@
+﻿using CocktailApplication.Presentation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CocktailApplication.Repository
+{
+  public interface IConsume
+    {
+
+        public async Task GetIngredients () 
+        { }
+    } 
+}
